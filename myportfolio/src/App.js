ab35ea18e4b277729b23a4a403bb52b780bb7ee2
alladijagadesh <126089112+alladijagadesh@users.noreply.github.com>
2024-10-24 22:56:59 -0400

    First Commit - Navbar

---
+++ myportfolio/src/App.js
@@ -0,0 +1,27 @@
+
+import './App.css';
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Experience from './components/Experience';
+import Education from './components/Education';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    {/*<About/>
+    <Experience/>
+    <Education/>
+    <Skills/>
+    <Projects/>
+    <Contact/>
+    <Footer/>*/}
+    </>
+  );
+}
+
+export default App;
